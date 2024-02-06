71fac00bdb6671c643882cd6e18cfd7085ed467f
Gaston Dicundo <73203907+gasdicundo@users.noreply.github.com>
2024-02-05 21:00:52 -0300

    Router corregido

--- src/router/router.js
+++ src/router/router.js
@@ -5,15 +5,16 @@ const chat = require('../controllers/chat.controller')
 const authController = require ('../controllers/auth.controller')
 const viewsTemplateController = require ('../controllers/views-template.controller')
 const usersController = require ('../controllers/users.controller')
+const authMiddleware = require('../middlewares/private-acces-middleware')
 
 const router = app => {
-    app.use('/api/products', productsController)
-    app.use('/api/carts', cartsController)
-    app.use('/addproduct', addproduct)
-    app.use('/chat', chat)
-    app.use('/api/auth', authController)
-    app.use('/', viewsTemplateController)
-    app.use('/api/users', usersController)
+    app.use('/api/products',authMiddleware, productsController)
+    app.use('/api/carts',authMiddleware, cartsController)
+    app.use('/addproduct',authMiddleware, addproduct)
+    app.use('/chat',authMiddleware, chat)
+    app.use('/api/auth',authMiddleware, authController)
+    app.use('/',authMiddleware, viewsTemplateController)
+    app.use('/api/users',authMiddleware, usersController)
 }
 
 module.exports = router
